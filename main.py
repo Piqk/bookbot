27bab3d0f66a6d396c9bc5549cf381fec559d4b5
piqk <danielestrada32123@gmail.com>
2025-03-27 23:36:30 -0600

    after comp restart

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from stats import main
+from stats import word_counter
+from stats import organizer
 
-def num_words():
-    book = main()
-    first_step = book.split()
-    number = 0
-    for word in first_step:
-        number += 1
-    print (f"{number} words found in the document")
+def get_book_text():
+    path = 'books/frankenstein.txt'
+    with open(path) as f:
+        contents = f.read()
+    return contents
 
-
-num_words()
-    
-
-
+print('============ BOOKBOT ============')
+print('Analyzing book found at books/frankenstein.txt...')
+print('----------- Word Count ----------')
+print(word_counter())
+print('--------- Character Count -------')
+data = organizer()
+sorted_data = sorted(data, key=lambda x: list(x.values())[0], reverse=True)
+for d in sorted_data:
+    for key, value in d.items():
+        print(f"{key}: {value}")
+print('============= END ===============')
 
--- stats.py
+++ stats.py
@@ -1,10 +1,43 @@
-def get_book_text(file_path):
-    with open(file_path) as f:
+def get_book_text():
+    path = 'books/frankenstein.txt'
+    with open(path) as f:
         contents = f.read()
     return contents
-    
 
-def main():
-    path = 'books/frankenstein.txt'
-    result = get_book_text(path)
-    return (result)
+def word_counter():
+    counter = 0
+    for i in get_book_text().split():
+        counter += 1 
+    return (f'Found {counter} total words')
+
+book = str(get_book_text())
+
+def count_char():
+    pairs = {}
+    for i in book.lower():
+        if i not in pairs:
+            n = 1
+            pairs.update({i: n})
+        elif i in pairs:
+            pairs[i] += 1
+    return pairs
+
+
+
+
+
+def organizer():
+    list_of_dict = [{k: v} for k, v in count_char().items()]
+    data = sorted(list_of_dict, key=lambda d: next(iter(d.values())), reverse=True)
+    return (data)
+
+
+
+
+
+   
+
+
+             
+
+
